b1a7d9e08446c0f94f629470c867fa9da47e4eb0
Цыварев Илья <cyvarev.ilya156@gmail.com>
2019-05-03 11:59:17 +0300

    Ввел функции для подключения к таблице, создал интерфейс

--- kursach2/Form1.Designer.cs
+++ kursach2/Form1.Designer.cs
@@ -28,35 +28,214 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id_client = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.birth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.polis = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cyvarevDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cyvarevDataSet = new kursach2.cyvarevDataSet();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.change_client = new System.Windows.Forms.Button();
+            this.add_client = new System.Windows.Forms.Button();
+            this.delete_client = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cyvarevDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cyvarevDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(167, 91);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id_client,
+            this.name,
+            this.birth,
+            this.polis});
+            this.dataGridView1.DataSource = this.cyvarevDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(1, -1);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(432, 452);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // id_client
+            // 
+            this.id_client.HeaderText = "id_client";
+            this.id_client.Name = "id_client";
+            // 
+            // name
+            // 
+            this.name.HeaderText = "name";
+            this.name.Name = "name";
+            // 
+            // birth
+            // 
+            this.birth.HeaderText = "birth";
+            this.birth.Name = "birth";
+            // 
+            // polis
+            // 
+            this.polis.HeaderText = "polis";
+            this.polis.Name = "polis";
+            // 
+            // cyvarevDataSetBindingSource
+            // 
+            this.cyvarevDataSetBindingSource.DataSource = this.cyvarevDataSet;
+            this.cyvarevDataSetBindingSource.Position = 0;
+            // 
+            // cyvarevDataSet
+            // 
+            this.cyvarevDataSet.DataSetName = "cyvarevDataSet";
+            this.cyvarevDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(544, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "id_client";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(544, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(544, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Date of birth";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(659, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(659, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(659, 119);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(659, 145);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(544, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Polis";
+            // 
+            // change_client
+            // 
+            this.change_client.Location = new System.Drawing.Point(515, 288);
+            this.change_client.Name = "change_client";
+            this.change_client.Size = new System.Drawing.Size(75, 23);
+            this.change_client.TabIndex = 9;
+            this.change_client.Text = "Change";
+            this.change_client.UseVisualStyleBackColor = true;
+            // 
+            // add_client
+            // 
+            this.add_client.Location = new System.Drawing.Point(596, 288);
+            this.add_client.Name = "add_client";
+            this.add_client.Size = new System.Drawing.Size(75, 23);
+            this.add_client.TabIndex = 10;
+            this.add_client.Text = "Add";
+            this.add_client.UseVisualStyleBackColor = true;
+            // 
+            // delete_client
+            // 
+            this.delete_client.Location = new System.Drawing.Point(677, 288);
+            this.delete_client.Name = "delete_client";
+            this.delete_client.Size = new System.Drawing.Size(75, 23);
+            this.delete_client.TabIndex = 11;
+            this.delete_client.Text = "Delete";
+            this.delete_client.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 448);
+            this.Controls.Add(this.delete_client);
+            this.Controls.Add(this.add_client);
+            this.Controls.Add(this.change_client);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cyvarevDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cyvarevDataSet)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.BindingSource cyvarevDataSetBindingSource;
+        private cyvarevDataSet cyvarevDataSet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_client;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn birth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn polis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button change_client;
+        private System.Windows.Forms.Button add_client;
+        private System.Windows.Forms.Button delete_client;
     }
 }
 
--- kursach2/Form1.cs
+++ kursach2/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,49 @@ namespace kursach2
         public Form1()
         {
             InitializeComponent();
+
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            string connectString = "Data Source=.\\silan.zyns.com:3306;" +
+                "Initial Catalog=cyvarev; " +
+                "Integrated Security=true;";
+
+            SqlConnection myConnection = new SqlConnection(connectString);
+
+            myConnection.Open();
+
+            string query = "SELECT * FROM Client";
+
+            SqlCommand command = new SqlCommand(query, myConnection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<string[]> data = new List<string[]>();
+
+            while (reader.Read())
+            {
+                data.Add(new string[3]);
+
+                data[data.Count - 1][0] = reader[0].ToString();
+                data[data.Count - 1][0] = reader[1].ToString();
+                data[data.Count - 1][0] = reader[2].ToString();
+            }
+
+            reader.Close();
+            myConnection.Close();
+
+            foreach (string[] s in data)
+                dataGridView1.Rows.Add(s);
         }
 
         private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
+
+        
     }
 }
